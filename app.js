d0153b6263e212857adb51fbeebf9602f33076de
Anna <anya@Annas-MacBook-Pro.local>
2019-10-22 16:44:06 -0700

    starting on game logic

--- app.js
+++ app.js
@@ -9,17 +9,3 @@ let board, turn, winner;
 /*------------Event Listners-----------*/
 
 /*------------Functions-----------*/
-function init(){
-    board = [
-        [0,0,0,0,0,0], //column1 (index0)
-        [0,0,0,0,0,0], //column2 (index0)
-        [0,0,0,0,0,0], //column3 (index0)
-        [0,0,0,0,0,0], //colum4 (index0)
-        [0,0,0,0,0,0], //column5 (index0)
-        [0,0,0,0,0,0], //column6 (index0)
-        [0,0,0,0,0,0], //column (index0)
-
-    ];
-    // calling on the board variable - each array in here represents each item in the board. 
-    // here we will create an array for each column
-}
