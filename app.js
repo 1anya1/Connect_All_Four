f872b832f8d1382c96b1b7de863f5c29cbd97835
Anna <anya@Annas-MacBook-Pro.local>
2019-10-23 17:46:48 -0700

    created columns

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 $(() => {
     // my constants
-    const player1=[];
-    const player2=[];
+    const player1={  //objects that will hold all of the data for the data
+        cells:[]
+        
+    };
+    const player2={
+        cells:[]
+    };
     var turn= 0;
     const rows=7;
     const columns=6
@@ -10,38 +15,56 @@ $(() => {
     const $container=$('<div>').addClass('container');
     $('body').append($container);
     
-    for (let row = 0; row < rows ; row++) {
-        const $row = $('<div>')
-          .addClass('row');
-        for (let col = 0; col < columns; col++) {
-          const $col = $('<div>')
-            .addClass('col empty')
-            .attr('id', col)
-            .attr('id', row);
-          $row.append($col);
-        }
-        $container.append($row);
-      };
+    // for (let row = 0; row < rows ; row++) {
+    //     const $row = $('<div>')
+    //       .addClass('row');
+    //     for (let col = 0; col < columns; col++) {
+    //       const $col = $(`<div id= "c${col}r${row}"></div>`)
+    //         .addClass('col empty')
+    //         // .attr('id', col)
+    //         // .attr('id', row);
+    //       $row.append($col);
+    //     }
+    //     $container.append($row);
+    //   };
+
+for (let col = 0; col < columns ; col++) {
+    const $col = $('<div>')
+      .addClass('col empty'+ col)
+    for (let row = 0; row < rows; row++) {
+      const $row = $(`<div id= "c${col}r${row}"></div>`)
+        .addClass('row')
+        // .attr('id', col)
+        // .attr('id', row);
+      $col.append($row);
+    }
+    $container.append($col);
+  };
 
     /// Event Handler for Moves//
     function turns () {
+    
     if(turn%2===0){
         // alert('player1');// how to create turns for each player?
     $(event.target).css('background-color', 'yellow').removeClass('empty').addClass('yellow');
-    // player1.push(parseInt($(event.target).attr('id')));
-    player1.push($(event.target).attr('id'));
-    player1.sort();
+    player1.cells.push($(event.target).attr('id'));
+    $(event.target).off('click');
+    // player1.push($(event.target).attr('id'));
+    player1.cells.sort();
     } else {
     $(event.target).css('background-color', 'red').removeClass('empty').addClass('red');
-    player2.push($(event.target).attr('id'));
-    player1.sort();
+    player2.cells.push($(event.target).attr('id'));
+    $(event.target).off('click');
+    player2.cells.sort();
     }
     turn++;
-console.log(player1)
-console.log(player2)
+    console.log(player1)
+    console.log(player2)
 
 }
-$('.empty').on('mouseover', turns ); //how to make sure that the class doesnt get reassignes and player is unable to click the play that was clicked. Also how to assign each one of the id divs to a matrix of array play
+$('.row').on('click', turns ); //how to make sure that the class doesnt get reassignes and player is unable to click the play that was clicked. Also how to assign each one of the id divs to a matrix of array play
 
 
 });
+
+
