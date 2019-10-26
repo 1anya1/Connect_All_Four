b687ccc692ff99b75e03e33c962d5b5a67041f45
Anna <anya@Annas-MBP.wavecable.com>
2019-10-26 06:59:40 -0700

    code updates

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 
 $(()=>{ ///on load function
-    // constants
-    const playersColors = {  // this is an object that will hold all of the players color plays. Our object Literal  look up datastructure
+   
+  // constants
+    const playersColors = {  // this is an object literal (datastructure)in the game that will assign players color plays. 
       '0': 'white',
       '1': 'yellow',
       '-1': 'red'
-    }
-    //variables
+    };
+    //variables  
     let board; 
     let turn;
-    let winner
+    let winner;
 
-    // element ref
+    // h2 element that will be used to display names and wins in the game 
     const messageElement = document.getElementById('message'); // global variable for message finds id of message
     
     //event listners
     // adding event listner on the column selector 
     document.querySelector('section.columnSelector') //this returns the DOM element
-    .addEventListener('click',handleClick );// creating event listner for the specific event
-    
+    .addEventListener('click', handleClick );// creating event listner for the specific event just like on click in jQuery 
+    // so essentially anytime someone clicks on the arrows above each column there is an event listner firing off
     //functions
-    initGame(); ///calling on the function since its a function declaration, init also calls the render that renders the board 
+    initGame(); ///calling on the function--function declaration, renders the board on reload 
     function initGame(){
       board = [
-        [0,0,0,0,0,0], //column 1 starts with bottom left (c0r0) foing all the way up tp (cor6)
+        [0,0,0,0,0,0], //column 1 starts with bottom left (c0r0) foing all the way up tp (cor5)
         [0,0,0,0,0,0], // column 2
         [0,0,0,0,0,0], // column 3
         [0,0,0,0,0,0], // column 4
@@ -31,26 +32,26 @@ $(()=>{ ///on load function
         [0,0,0,0,0,0], // column 6
         [0,0,0,0,0,0]  // column 7
       ];
-      turn = 1;
-      winner = null; //1 -1 null and T for the tie for potential values 
+      turn = 1; // game alsways starts off with player one " yellow turn"
+      winner = null; //1 for yellow -1 for red and null for no winner null  
       renderBoard(); //calling on function render 
     }
     
     function renderBoard () {  /// from state to dom transfer , render the board for each nested column render the board 
-      board.forEach(function(columnArr, columnIdx){
+      board.forEach(function(columnArr, columnIdx){ //forEach() calls a provided callback function once for each element in an array in ascending order.
       // hide or show columns marker depending if there are zeros or not ( if the column is full disable the triangle)
-      const marker = document.getElementById(`column${columnIdx}`)
-      if(columnArr.indexOf(0)=== -1){ // this represents a full column.. eek column is full 
-        marker.style.visibility = 'hidden'; // this hides the column triangle if it is full 
+      const marker = document.getElementById(`column${columnIdx}`) //using template literals to get the column index -- this is for the top partt of the gmae the triangles 
+      if(columnArr.indexOf(0)=== -1){ // this represents a full column. The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present. 
+        marker.style.visibility = 'hidden'; // this hides the column triangle if the column array is full 
       } else{
-        marker.style.visibility = 'visible';
+        marker.style.visibility = 'visible';// the triangle still shows if the array is not full ( meaning not -1)
       }
       //looping through the arrays in the board, using two parameters colmn array and extracting the index of the column array 
-      columnArr.forEach(function(cellVal, rowIndex ){
+      columnArr.forEach(function(boxVal, rowIndex ){
         //going through each column at a time and checking each one of the cell values. 
         const div = document.getElementById(`c${columnIdx}r${rowIndex}`); //getting the id's 
         // console.log(div);
-        div.style.backgroundColor = playersColors[cellVal]; // this renders the board 
+        div.style.backgroundColor = playersColors[boxVal]; // this calls on the object literal and assigns the color to the div on which the player pressed
         });
       
     });
@@ -60,11 +61,11 @@ $(()=>{ ///on load function
         messageElement.textContent = "it's a tie"
     }
     else {
-      messageElement.textContent = `${playersColors[winner].toUpperCase()}'s Wins`;//calling on the object of Players Colors  using square bracket notation // need to uppercase this 
+      messageElement.textContent = `${playersColors[winner].toUpperCase()} Wins`;//calling on the object of Players Colors  using square bracket notation // need to uppercase this 
         }
     }else {
       // messageElement.textContent = `${turn}'s Turn`;//calling on the turn of player 1 or -1;
-      messageElement.textContent = `${playersColors[turn].toUpperCase()}'s Turn`;//calling on the object of Players Colors  using square bracket notation // need to uppercase this 
+      messageElement.textContent = `${playersColors[turn].toUpperCase()} Turn`;//calling on the object of Players Colors  using square bracket notation // need to uppercase this 
      }
     
     };
